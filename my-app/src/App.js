6769c792f3ccb159aa71caae4cef6b8ce203494b
Risako Shirasu <shirasu@S678.local>
2021-06-01 16:01:11 +0900

    色変更設定

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,10 +7,13 @@ function App() {
 
   const increment = () => {
     setCount(count + 1);
+    setRed(true);
   };
 
   return (
     <div className="app">
+      {/* ?はtrue, : はelseを表す */}
+      <h1 className={isRed ? "red" : ""}>Change my color!!!!!</h1>
       <button onClick={increment}>Increment</button>
       <h1>{count}</h1>
     </div>
