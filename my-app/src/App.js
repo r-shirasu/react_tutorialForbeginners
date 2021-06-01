5ea9f37fa8b1e9787e8cfb5e4d2def91c9d7e88a
Risako Shirasu <shirasu@S678.local>
2021-06-01 15:15:01 +0900

    randor tweet

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
+import Tweet from "./Tweet";
 
 function App() {
-  const sayHello = () => {
-    console.log("hello");
-  };
-
   return (
     <div>
       <h1>Hello, react</h1>
-      <button onClick={sayHello}>Hello</button>
+      <Tweet />
     </div>
   );
 }
