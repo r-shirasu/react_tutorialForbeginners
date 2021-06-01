f84aa2d1d93d88c406fcf3ed1bcd7db0a3b3d183
Risako Shirasu <shirasu@S678.local>
2021-06-01 15:42:32 +0900

    messageもpropで

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,10 @@ import Tweet from "./Tweet";
 function App() {
   return (
     <div className="app">
-      <Tweet name="spring" />
-      <Tweet name="summer" />
-      <Tweet name="autmun" />
-      <Tweet name="winter" />
+      <Tweet name="spring" message="春だね" />
+      <Tweet name="summer" message="夏だね" />
+      <Tweet name="autmun" message="秋だね" />
+      <Tweet name="winter" message="冬だね" />
     </div>
   );
 }
--- my-app/src/Tweet.js
+++ my-app/src/Tweet.js
@@ -5,7 +5,7 @@ function Tweet(props) {
   return (
     <div className="tweet">
       <h3>{props.name}</h3>
-      <p>This is a random tweet</p>
+      <p>{props.message}</p>
       <h3>Number of likes</h3>
     </div>
   );
